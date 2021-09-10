264671108c35d77f92df069e26d75fa384f199c5
sairish2001 <sairish2001@gmail.com>
2021-09-10 23:37:01 +0530

    team section added and UI improvement

--- js/index.js
+++ js/index.js
@@ -3,3 +3,18 @@ function sendmail(){
     var message=document.getElementById('message').value;
     window.location = "mailto:dscgndu@gmail.com?subject="+subject+"&body="+message;
 }
+function loadteam(){
+    var data='';
+    for(var i=0;i<memberdata.length;i++){
+        data+=`<div class="mdl-cell mdl-cell--4-col teamcard">
+        <img src="${memberdata[i]['picture']}" alt="" class="teampic">
+        <div class="teammembername">${memberdata[i]['name']}</div>
+        <div class="positionmember">${memberdata[i]['posiiton']}</div>
+        <a class="mdl-button mdl-js-button mdl-button--icon colorlinkedinanchor" href="${memberdata[i]['linkedin']}" target="_blank">
+            <i class="fa fa-linkedin colorlinkedin"></i>
+        </a>
+    </div>`;
+    }
+    document.getElementById('memberdatagrid').innerHTML=data;
+}
+loadteam();
---
+++ js/memberdata.js
@@ -0,0 +1,14 @@
+var memberdata=[
+    {"posiiton":"GDSC Lead", "name": "Rajinderpal Singh", "linkedin":"https://www.linkedin.com/in/rajinderpalsingh2001/", "picture": "members/rajinderpal_singh.jpg"},
+    {"posiiton":"Web Development Lead", "name": "Anhat Singh", "linkedin":"https://www.linkedin.com/in/anhatsingh/", "picture": "members/anhat_singh.jpg"},
+    {"posiiton":"Web Development Co-Lead", "name": "Shrey Saggar", "linkedin":"https://www.linkedin.com/in/shrey-saggar-985b761b3/", "picture": "members/shrey_saggar.jpg"},
+    {"posiiton":"AI/ML Lead", "name": "Vaibhav Arora", "linkedin":"https://www.linkedin.com/in/vaibhavarora102/", "picture": "members/vaibhav_arora.jpg"},
+    {"posiiton":"AI/ML Co-Lead", "name": "Ashwinder Singh", "linkedin":"https://www.linkedin.com/in/ashwinder-singh-93422a1b1/", "picture": "members/ashwinder_singh.jpg"},
+    {"posiiton":"Mobile App Development Lead", "name": "Japsimran Singh", "linkedin":"https://www.linkedin.com/in/japsimransingh13/", "picture": "members/japsimran_singh.jpg"},
+    {"posiiton":"CP Lead", "name": "Devang Chopra", "linkedin":"https://www.linkedin.com/in/devang-chopra-a7463a12a/", "picture": "members/devang_chopra.jpg"},
+    {"posiiton":"CP Co-Lead", "name": "Riya Rajput", "linkedin":"https://www.linkedin.com/in/riya-rajput-23400a200/", "picture": "members/riya_rajput.jpg"},
+    {"posiiton":"Creative and Marketing Lead", "name": "Akhil Aggarwal", "linkedin":"https://www.linkedin.com/in/akhilbansalaggarwal/", "picture": "members/akhil_aggarwal.jpg"},
+    {"posiiton":"Creative and Marketing Co-Lead", "name": "Cheshta Handa", "linkedin":"https://www.linkedin.com/in/rajinderpalsingh2001/", "picture": "members/cheshta_handa.jpg"},
+    {"posiiton":"PR and Event Management Lead", "name": "Anchal Katoch", "linkedin":"https://www.linkedin.com/in/anchal-katoch-12069b1b2/", "picture": "members/anchal_katoch.jpg"},
+    {"posiiton":"PR and Event Management Co-Lead", "name": "Priyanshu Dhawan", "linkedin":"https://www.linkedin.com/in/priyanshuthe1/", "picture": "members/priyanshu_dhawan.jpg"},
+]
